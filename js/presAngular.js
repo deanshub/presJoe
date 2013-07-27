da1dc6e0fb494e459207985b8d3668936351974f
dean shub <deanshub@gmail.com>
2013-07-27 21:06:44 +0300

    moving presentations around to the server side

--- js/presAngular.js
+++ js/presAngular.js
@@ -47,8 +47,9 @@ controllers.navbarController = function ($scope) {
 };
 
 controllers.simpleCtrl = function ($scope, $http) {
-	$http.get('api_mock/presentations.json').success(function(data) {
+	$http.get('presJoe/public/presentations').success(function(data) {
 		$scope.presentations = data;
+		console.log(data);
 	});
 
 	$scope.addPresentation = function() {
